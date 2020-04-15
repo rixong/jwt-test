b6d91886bfa94a4e6690b268b71aad2c4f137b28
rixong <rixong@gmail.com>
2020-04-15 13:12:06 -0500

    broke api

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -10,33 +10,39 @@ class ReposController < ApplicationController
     uri = URI.parse(url)
     response = Net::HTTP.get_response(uri)
     result = JSON.parse(response.body)
+      ### Checks if request is valid URI and there is content
     if response.message != 'OK' || result.count == 0 
       render json: {message: "Not a valid Github Username"}
     else
-      if !Repo.find_by(git_username: user_name)
+        ### Checks if Timeline already exists for current user, if not it creates one
+      tl = Timeline.where(['name = ? and user_id = ?',user_name, current_user.id])
+      if tl.empty?
+        puts 'New timeline  !XXXXXXXXXXXXXXXXXXXXXXXXXXX'
         tl = Timeline.create(name: user_name, user_id: current_user.id)
-        result.each do |repo|
+      end
+      # pry
+        ## Creates a new repo if doesn't exist
+      result.each do |repo|
+          ### if repo doesn't exist, create it
+        if !Repo.find_by(git_id: repo['id'])
           rp = Repo.create(
             git_id: repo['id'], 
-            name: repo['name'], 
+            name: repo['name'],
             git_username: user_name,
             html_url: repo['html_url'],
             branches_url: repo['branches_url'],
             repo_created_at: repo['created_at'],
             repo_updated_at: repo['updated_at']
           )
-          TimelineRepo.create(timeline_id: tl.id,repo_id: rp.id)
         end
+          ##Create unique TLR for each repo (for curUser)
+          puts "XXXXXXXX #{tl[0].id} .. #{repo['id']}"
+        TimelineRepo.create(timeline_id: tl[0].id, repo_id: repo['id'])
       end
       repos = Repo.where("git_username = ?", user_name)
       render json: {message: "OK!", result: repos}
+      # render json: {message: "OK!"}
     end
-  end
-
-  def index
-
-    
-
   end
 
 end
