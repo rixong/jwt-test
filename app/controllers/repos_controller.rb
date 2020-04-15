01b7c146c5c74259ab2f85d19d2d5775475023c3
rixong <rixong@gmail.com>
2020-04-15 08:38:40 -0500

    timeline creation uniqueness test

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -5,21 +5,21 @@ require 'json'
 class ReposController < ApplicationController
 
   def create
-    
-    url = "https://api.github.com/users/#{params[:github_username]}/repos"
+    user_name = params[:github_username].downcase
+    url = "https://api.github.com/users/#{user_name}/repos"
     uri = URI.parse(url)
     response = Net::HTTP.get_response(uri)
     result = JSON.parse(response.body)
     if response.message != 'OK' || result.count == 0 
       render json: {message: "Not a valid Github Username"}
     else
-      if !Repo.find_by(git_username: params[:github_username])
-        tl = Timeline.create(name: params[:github_username], user_id: current_user.id)
+      if !Repo.find_by(git_username: user_name)
+        tl = Timeline.create(name: user_name, user_id: current_user.id)
         result.each do |repo|
           rp = Repo.create(
             git_id: repo['id'], 
             name: repo['name'], 
-            git_username: repo['owner']['login'],
+            git_username: user_name,
             html_url: repo['html_url'],
             branches_url: repo['branches_url'],
             repo_created_at: repo['created_at'],
@@ -28,7 +28,7 @@ class ReposController < ApplicationController
           TimelineRepo.create(timeline_id: tl.id,repo_id: rp.id)
         end
       end
-      repos = Repo.where("git_username = ?", params[:github_username])
+      repos = Repo.where("git_username = ?", user_name)
       render json: {message: "OK!", result: repos}
     end
   end
